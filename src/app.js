ee8bc86e579d9c9f00d8dbf065f314421a4233e2
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-08-17 15:44:16 -0300

    feat: Criado router user e procuct

--- src/app.js
+++ src/app.js
@@ -1,29 +1,11 @@
 import express from 'express';
+import { userRouter } from './modules/user/user.controller.js';
+import { productRouter } from './modules/products/product.controller.js';
 
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World teste com eslint');
-});
-
-app.get('/user', function (req, res) {
-  res.send({
-    nome: 'Haroldo',
-  });
-});
-
-app.get('/user/:nome', function (req, res) {
-  res.send({
-    nome: req.params.nome,
-  });
-});
-
-app.get('/user/:nome/sobrenome/:sobrenome', function (req, res) {
-  res.send({
-    nome: req.params.nome,
-    sobrenome: req.params.sobrenome,
-  });
-});
+app.use(userRouter);
+app.use(productRouter);
 
 app.listen(8080, function () {
   console.log('Servidor rodando');
