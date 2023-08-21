e20bae1d314615dfbd0c49e54960bc96634a6f08
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-08-21 15:46:48 -0300

    feat: Inserido tipagem nas classes e criado DTO

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-inferrable-types': 'error',
   },
