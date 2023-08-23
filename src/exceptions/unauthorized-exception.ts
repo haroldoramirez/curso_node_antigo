899fcba26f25b814af8fff14dd395a0e80cd33ad
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-08-23 14:48:17 -0300

    feat: Tratamento de exceptions 2

--- src/exceptions/unauthorized-exception.ts
+++ src/exceptions/unauthorized-exception.ts
@@ -1,9 +1,7 @@
-import { AppException } from "./app-exception";
+import { AppException } from './app-exception';
 
 export class UnauthorizedException extends AppException {
-
-    constructor() {
-        super('User without permission');
-    }
-
+  constructor() {
+    super('User without permission');
+  }
 }
