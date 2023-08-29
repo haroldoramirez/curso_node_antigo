371c8fa127991a20d26e1be3ae7d3c1b2d8dbb0d
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-08-29 00:03:04 -0300

    feat: Criado Auth inicial 2

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
-import { Request, Response, Router } from "express";
-import { AuthDTO } from "./dtos/auth.dto";
-import { validateAuth } from "./auth.service";
-import { ReturnError } from "@exceptions/dtos/return-error-dto";
+import { Request, Response, Router } from 'express';
+import { AuthDTO } from './dtos/auth.dto';
+import { validateAuth } from './auth.service';
+import { ReturnError } from '@exceptions/dtos/return-error-dto';
 
 const authRouter = Router();
 
@@ -9,13 +9,15 @@ const router = Router();
 
 authRouter.use('/auth', router);
 
-router.post('/', async (req: Request<undefined, undefined, AuthDTO>, res: Response): Promise<void> => {
-  
-    const user = await validateAuth(req.body).catch((error => {
-        new ReturnError(res, error);
-    }));
+router.post(
+  '/',
+  async (req: Request<undefined, undefined, AuthDTO>, res: Response): Promise<void> => {
+    const user = await validateAuth(req.body).catch((error) => {
+      new ReturnError(res, error);
+    });
 
     res.send(user);
-});
+  },
+);
 
 export default authRouter;
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { UserModel } from "@modules/user/user.model";
-import { AuthDTO } from "./dtos/auth.dto";
-import { getUserByEmail } from "@modules/user/user.service";
+import { UserModel } from '@modules/user/user.model';
+import { AuthDTO } from './dtos/auth.dto';
+import { getUserByEmail } from '@modules/user/user.service';
 
 export const validateAuth = async (authDTO: AuthDTO): Promise<UserModel> => {
-    const user = getUserByEmail(authDTO.email);
+  const user = getUserByEmail(authDTO.email);
 
-    return user;
-}
+  return user;
+};
