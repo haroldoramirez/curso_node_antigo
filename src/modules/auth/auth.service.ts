8a5651abb8bf8a9507e1cc6a4211ab93008e7eaa
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-08-29 15:13:06 -0300

    feat: logica inicial de autenticacao

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { UserModel } from '@modules/user/user.model';
 import { AuthDTO } from './dtos/auth.dto';
 import { getUserByEmail } from '@modules/user/user.service';
+import { validatePassword } from '@utils/password';
+import { NotFoundException } from '@exceptions/not-found-exception';
 
 export const validateAuth = async (authDTO: AuthDTO): Promise<UserModel> => {
-  const user = getUserByEmail(authDTO.email);
+  const user = await getUserByEmail(authDTO.email);
+
+  const isValidPassword = await validatePassword(authDTO.password, user.password);
+
+  if (!isValidPassword) {
+    throw new NotFoundException('User');
+  }
 
   return user;
 };
