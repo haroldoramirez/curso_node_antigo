49737cec76e9fe8855961e670700290ef2f60edb
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-08-29 00:02:02 -0300

    feat: Criado Auth inicial

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { userRouter } from './modules/user/user.controller';
 import { productRouter } from './modules/products/product.controller';
+import authRouter from '@modules/auth/auth.controller';
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(authRouter);
 app.use(productRouter);
 
 app.listen(8080, (): void => {
